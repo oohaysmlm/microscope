8014ddcedb6fefe271d1140e48b7619e36060479
Steven Kehoe <oohaysmlm@gmail.com>
2015-06-20 15:47:23 -0400

    Setup a  helper on the

---
+++ client/templates/posts/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Steven on 6/20/2015.
+ */
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});

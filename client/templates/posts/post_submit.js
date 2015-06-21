0d3cebf21e541ac690bf55b0033eb3ea6117b69c
Steven Kehoe <oohaysmlm@gmail.com>
2015-06-20 22:28:52 -0400

    Enforce post URL uniqueness

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,6 +1,3 @@
-/**
- * Created by Steven on 6/20/2015.
- */
 Template.postSubmit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -8,7 +5,14 @@ Template.postSubmit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         };
-        post._id = Posts.insert(post);
-        Router.go('postPage', post);
+        Meteor.call('postInsert', post, function(error, result) {
+            // display the error to the user and abort
+            if (error)
+                return alert(error.reason);
+            // show this result but route anyway
+            if (result.postExists)
+                alert('This link has already been posted');
+            Router.go('postPage', {_id: result._id});
+        });
     }
 });

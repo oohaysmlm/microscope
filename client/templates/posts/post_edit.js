c0074cff7d7c1d7a914c037544918b0055671a06
Steven Kehoe <oohaysmlm@gmail.com>
2015-06-21 12:35:03 -0400

    Validate post contents when editing

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -2,6 +2,17 @@
  * Created by Steven on 6/21/2015.
  */
 
+Template.postEdit.onCreated(function() {
+    Session.set('postEditErrors', {});
+});
+Template.postEdit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postEditErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+    }
+});
 Template.postEdit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -10,6 +21,9 @@ Template.postEdit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         }
+        var errors = validatePost(postProperties);
+        if (errors.title || errors.url)
+            return Session.set('postEditErrors', errors);
         Posts.update(currentPostId, {$set: postProperties}, function(error) {
             if (error) {
                 // display the error to the user

0c56a8ec8db8f535db7a9f4b7d16b82e2b1542fa
Steven Kehoe <oohaysmlm@gmail.com>
2015-06-22 07:52:21 -0400

    Added comments collection pub/sub and fixtures

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,11 @@
 /**
  * Created by Steven on 6/20/2015.
  */
+
 Meteor.publish('posts', function() {
     return Posts.find();
+});
+
+Meteor.publish('comments', function() {
+    return Comments.find();
 });

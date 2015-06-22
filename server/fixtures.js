badb92fe512d22acc11b58d1ddbe4c21e90fcdd8
Steven Kehoe <oohaysmlm@gmail.com>
2015-06-22 08:52:09 -0400

    Denormalized the number of comments into the post

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,7 +1,3 @@
-/**
- * Created by Steven on 6/20/2015.
- */
-// Fixture data
 if (Posts.find().count() === 0) {
     var now = new Date().getTime();
     // create two users
@@ -18,7 +14,8 @@ if (Posts.find().count() === 0) {
         userId: sacha._id,
         author: sacha.profile.name,
         url: 'http://sachagreif.com/introducing-telescope/',
-        submitted: new Date(now - 7 * 3600 * 1000)
+        submitted: new Date(now - 7 * 3600 * 1000),
+        commentsCount: 2
     });
     Comments.insert({
         postId: telescopeId,
@@ -27,7 +24,6 @@ if (Posts.find().count() === 0) {
         submitted: new Date(now - 5 * 3600 * 1000),
         body: 'Interesting project Sacha, can I get involved?'
     });
-
     Comments.insert({
         postId: telescopeId,
         userId: sacha._id,
@@ -40,13 +36,15 @@ if (Posts.find().count() === 0) {
         userId: tom._id,
         author: tom.profile.name,
         url: 'http://meteor.com',
-        submitted: new Date(now - 10 * 3600 * 1000)
+        submitted: new Date(now - 10 * 3600 * 1000),
+        commentsCount: 0
     });
     Posts.insert({
         title: 'The Meteor Book',
         userId: tom._id,
         author: tom.profile.name,
         url: 'http://themeteorbook.com',
-        submitted: new Date(now - 12 * 3600 * 1000)
+        submitted: new Date(now - 12 * 3600 * 1000),
+        commentsCount: 0
     });
 }

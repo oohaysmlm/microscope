f1f52ea6a22782dc8b3c25bf47afd1a2672a46f5
Steven Kehoe <oohaysmlm@gmail.com>
2015-06-26 01:19:50 -0400

    Added basic upvoting algorithm

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,3 +1,4 @@
+// Fixture data
 if (Posts.find().count() === 0) {
     var now = new Date().getTime();
     // create two users
@@ -15,7 +16,9 @@ if (Posts.find().count() === 0) {
         author: sacha.profile.name,
         url: 'http://sachagreif.com/introducing-telescope/',
         submitted: new Date(now - 7 * 3600 * 1000),
-        commentsCount: 2
+        commentsCount: 2,
+        upvoters: [],
+        votes: 0
     });
     Comments.insert({
         postId: telescopeId,
@@ -32,21 +35,35 @@ if (Posts.find().count() === 0) {
         body: 'You sure can Tom!'
     });
     Posts.insert({
+        title: 'Meteor',
+        userId: tom._id,
+        author: tom.profile.name,
+        url: 'http://meteor.com',
+        submitted: new Date(now - 10 * 3600 * 1000),
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
+    });
+    Posts.insert({
         title: 'The Meteor Book',
         userId: tom._id,
         author: tom.profile.name,
         url: 'http://themeteorbook.com',
         submitted: new Date(now - 12 * 3600 * 1000),
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
     });
-    for (var i = 0; i < 20; i++) {
+    for (var i = 0; i < 10; i++) {
         Posts.insert({
             title: 'Test post #' + i,
             author: sacha.profile.name,
             userId: sacha._id,
             url: 'http://google.com/?q=test-' + i,
-            submitted: new Date(now - i * 3600 * 1000),
-            commentsCount: 0
+            submitted: new Date(now - i * 3600 * 1000 + 1),
+            commentsCount: 0,
+            upvoters: [],
+            votes: 0
         });
     }
 }
